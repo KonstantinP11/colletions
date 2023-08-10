b033a332963bb1980c9f1a4566b02cbc7167f176
conditional <105pkn@mail.ru>
2023-08-10 21:43:09 +0700

    домашка готова

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
+import service.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(8, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        Service.printOddNumber(nums);
+        System.out.println();
+        Service.printEvenNumber(nums);
+
+        List<String> words = new ArrayList<>(List.of("kod", "word", "kod", "list", "test", "test"));
+        Service.printWords(words);
+
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три", "два", "три"));
+        Service.printCountingDuplicates(strings);
     }
 }
